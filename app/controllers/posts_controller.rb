e10e7415b804735b7f594251971d7efd96e12012
lucas nunes de sousa <lucasnshunter@hotmail.com>
2016-10-31 17:03:20 +0000

    criado views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,14 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+     
     @posts = Post.most_recent.paginate(page: params[:page],per_page: 4)
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    
   end
 
   # GET /posts/new
